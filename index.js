d04b0bb51197b69732cdd91183a7fe04333bf193
yanglin <richenlin@gmail.com>
2017-11-22 09:18:03 +0800

    update version

--- index.js
+++ index.js
@@ -32,6 +32,9 @@ const exec = function (app, ctx, options, group, controller, action) {
         } else {
             cls = caches[controller];
         }
+        if (!cls) {
+            ctx.throw(404, 'Controller not found.');
+        }
         instance = new cls(ctx, app);
     } catch (e) {
         ctx.throw(404, app.app_debug ? e : `Controller ${group ? group + '/' : ''}${controller} not found.`);
